ce65db1ab25a25d8777129f95975c2bb794dd4e6
蒼時弦也 <elct9620@frost.tw>
2021-12-18 01:01:57 +0800

    Remove unncessary Boxing::Dependency

--- lib/boxing.rb
+++ lib/boxing.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'boxing/version'
-require_relative 'boxing/dependency'
 require_relative 'boxing/package'
 require_relative 'boxing/database'
 require_relative 'boxing/command'
