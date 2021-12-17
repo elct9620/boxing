b107f74d3ab433da0d114396679145fabf0a265b
蒼時弦也 <elct9620@frost.tw>
2021-12-17 23:33:06 +0800

    Add mode and compare feature to Boxing::Package

--- lib/boxing/package.rb
+++ lib/boxing/package.rb
@@ -1,20 +1,70 @@
 # frozen_string_literal: true
 
+require 'yaml'
+
 module Boxing
   # The rubygems mapping for Linux package
   #
   # @since 0.1.0
   class Package
+    class << self
+      # Load Required Packages
+      #
+      # @return [Boxing::Package]
+      #
+      # @since 0.1.0
+      def load(path)
+        data = YAML.safe_load(File.read(path))
+        mode = 0b00
+        mode |= Package::RUNTIME if data['runtime']
+        mode |= Package::BUILD if data['build']
+        new(data['name'], data['version'], mode: mode)
+      end
+    end
+
+    include Comparable
+
+    # @since 0.1.0
+    RUNTIME = 0b01
+
+    # @since 0.1.0
+    BUILD = 0b10
+
     # @since 0.1.0
     attr_reader :name, :version
 
     # @param name [String] the package name
     # @param version [String] the package version
+    # @param mode [Number] is runtime or build
     #
     # @since 0.1.0
-    def initialize(name, version = nil)
+    def initialize(name, version = nil, mode: RUNTIME)
       @name = name
       @version = version
+      @mode = mode
+    end
+
+    # @return [TrueClass|FalseClass] is for build
+    #
+    # @since 0.1.0
+    def build?
+      @mode & BUILD == BUILD
+    end
+
+    # @return [TrueClass|FalseClass] is for runtime
+    #
+    # @since 0.1.0
+    def runtime?
+      @mode & RUNTIME == RUNTIME
+    end
+
+    # Compare is same package
+    #
+    # @return [TrueClass|FalseClass]
+    #
+    # @since 0.1.0
+    def ==(other)
+      @name == other.name
     end
 
     # Convert to string
--- spec/boxing/package_spec.rb
+++ spec/boxing/package_spec.rb
@@ -3,6 +3,40 @@
 RSpec.describe Boxing::Package do
   subject(:package) { described_class.new('openssl-dev', '~1.1.1') }
 
+  describe '#build?' do
+    it { is_expected.not_to be_build }
+
+    context 'when mark as build' do
+      subject { described_class.new('openssl-dev', '~1.1.1', mode: 0b10) }
+
+      it { is_expected.to be_build }
+    end
+  end
+
+  describe '#runtime?' do
+    it { is_expected.to be_runtime }
+
+    context 'when mark as build only' do
+      subject { described_class.new('openssl-dev', '~1.1.1', mode: 0b10) }
+
+      it { is_expected.not_to be_runtime }
+    end
+  end
+
+  describe '#==' do
+    subject { package == other }
+
+    let(:other) { described_class.new('openssl-dev', '~1.2.1') }
+
+    it { is_expected.to be_truthy }
+
+    context 'when package is different' do
+      let(:other) { described_class.new('openssl', '~1.1.1') }
+
+      it { is_expected.to be_falsy }
+    end
+  end
+
   describe '#to_s' do
     subject { package.to_s }
 
