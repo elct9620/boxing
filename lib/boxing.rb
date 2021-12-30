13a86b698b5fb61bbd94e1c63a4e8a7563e59f22
蒼時弦也 <elct9620@frost.tw>
2021-12-30 18:28:19 +0800

    Add Boxing::Config

--- lib/boxing.rb
+++ lib/boxing.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'boxing/version'
+require_relative 'boxing/config'
 require_relative 'boxing/package'
 require_relative 'boxing/database'
 require_relative 'boxing/context'
@@ -10,6 +11,8 @@ require_relative 'boxing/command'
 #
 # @since 0.1.0
 module Boxing
+  LOCK = Mutex.new
+
   module_function
 
   # @return [Bundler::Dependency]
@@ -20,5 +23,20 @@ module Boxing
       .definition
       .current_dependencies
       .select { |dep| (dep.groups & groups).any? }
+  end
+
+  # @return [Boxing::Config]
+  #
+  # @since 0.5.0
+  def config(&block)
+    return @config if @config
+
+    LOCK.synchronize do
+      return @config if @config
+
+      @config = Config.new(&block)
+    end
+
+    @config
   end
 end
