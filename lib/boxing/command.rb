cc99031d8a969a3c56527f8e67e01ec214403e36
Aotokitsuruya <contact@aotoki.me>
2023-12-29 21:04:07 +0800

    Replace Thor with Rake

--- lib/boxing/command.rb
+++ lib/boxing/command.rb
@@ -1,20 +1,28 @@
 # frozen_string_literal: true
 
-require 'thor'
+require 'rake'
 
 module Boxing
   # The Main Command
   #
   # @since 0.1.0
-  class Command < Thor
-    # :nodoc:
-    def self.exit_on_failure?
-      true
+  class Command < Rake::Application
+    # @since 0.11.0
+    TASK_ROOT = File.expand_path('./tasks', __dir__)
+
+    # @see Rake::Application#run
+    def run(argv = ARGV)
+      standard_exception_handling do
+        init('boxing', argv)
+        load_rakefile
+        top_level
+      end
     end
 
-    require_relative 'commands/base'
-    require_relative 'commands/generate'
-    require_relative 'commands/compose'
-    require_relative 'commands/update'
+    # @see Rake::Application#load_rakefile
+    def load_rakefile
+      glob("#{TASK_ROOT}/**/*.rake").each { |task| add_import task }
+      load_imports
+    end
   end
 end
